d4c043cb14842afa88f8ab2eee5d22d7968c9a14
Andrew Moore <a.moore@pbsa.info>
2018-03-21 21:24:10 -0300

    Added endpoint to return a paginated list of blocks. Caching blocks that need to be retrieve from the BlockChain.

--- rest/blocks.js
+++ rest/blocks.js
@@ -110,6 +110,189 @@ module.exports = function (blockchainAPI) {
   }
 
   /**
+   * Retrieve the block from the blockchain.
+   * 
+   * @param {number} blockNumber 
+   * @returns 
+   */
+  function getBlockFromBlockchain (blockNumber) {
+
+    // Make sure the blockNumber is valid.
+    // Throwing an error here so that the promise all catch will be able to handle it.
+    if (isNaN(blockNumber)) {
+      throw new Error('Invalid block number.');
+    }
+
+    return blockchainAPI.db_api()
+      .exec('get_block', [blockNumber])
+      .then((block) => {
+        
+        if (!block) {
+          throw new Error(BLOCK_NOT_FOUND);
+        }
+        
+        block._id = blockNumber;
+
+        return new Promise((resolve, reject) => {
+
+          // Save the block to the database.
+          saveBlock(block, (error) => {
+
+            if (error) {
+              return reject(new Error('Failed to save block.'));
+            }
+
+            return resolve(block);
+
+          });
+
+        });
+
+      });
+  }
+
+  /**
+   * Request all of the blocks from the blockchain.
+   * 
+   * @param {number[]} blockNumbers 
+   * @param {function} callback 
+   * @returns 
+   */
+  function getBlocksFromBlockchain (blockNumbers, callback) {
+
+    var requests = [];
+
+    // Allow them to pass either a single block number or an array.
+    if (!Array.isArray(blockNumbers)) {
+      blockNumbers = [blockNumbers];
+    }
+
+    // If any of the block numbers are not actually numbers throw an error.
+    if (blockNumbers.some(item => isNaN(item))) {
+      return callback(new Error('Some of the blocknumbers are invalid.'));
+    }
+
+    // Loop through each block and build an array of the promises.
+    for (let i = 0, len = blockNumbers.length; i < len; i++) {
+      requests.push(getBlockFromBlockchain(blockNumbers[i]));
+    }
+
+    // Wait for all the promises to resolve before calling the callback.
+    Promise.all(requests).then((blocks) => {
+      return callback(null, blocks);
+    })
+    .catch((error) => {
+      return callback(error);
+    });
+
+  }
+
+  /**
+   * Save the block to Mongo.
+   * 
+   * @param {object} block 
+   * @param {function} callback 
+   */
+  function saveBlock (block, callback) {
+
+    // Connect to Mongo to first check if we already have this block.
+    MongoClient.connect(config.MONGO, (error, database) => {
+
+      var collection;
+
+      if (error) {
+        return callback(error);
+      }
+
+      // Select the blocks collection.
+      collection = database.collection('blocks');
+
+      // Insert the block into Mongo.
+      collection.insertOne(block, (error) => {
+
+        if (error) {
+          return callback(error);
+        }
+
+        // Return the Fresh block.
+        return callback(null, block);
+
+      });
+
+    });
+
+  }
+
+  /**
+   * Get a list of blocks from the blockchain.
+   */
+  blocksRouter.route('/')
+    .get((req, res) => {
+
+      MongoClient.connect(config.MONGO, (error, db) => {
+
+        var start = 1;
+        var limit = 10;
+        var missing = [];
+
+        if (error) {
+          return errors.handleResponse(res, error.message);
+        }
+  
+        // Make sure the params are numbers.
+        if (!isNaN(req.query.start)) {
+          start = parseInt(req.query.start, 10);
+        }
+
+        if (!isNaN(req.query.limit)) {
+          limit = parseInt(req.query.limit, 10);
+        }
+
+        // Update the specific dynamic global document in the meta collection.
+        db.collection('blocks')
+          .find({
+            _id: {
+              '$gte': start,
+              '$lt': limit + start
+            }
+          })
+          .toArray((error, blocks) => {
+          
+            if (error) {
+              return errors.handleResponse(res, error.message);
+            }
+            
+            // Loop through and validate that all the proper items are there, if not request them from the block chain.
+            for (let i = start, end = limit + start; i < end; i++) {
+              let block = blocks.find(item => item._id === i);
+
+              // If the block isn't in the results then it is missing and we should retrieve it from the blockchain.
+              if (!block) {
+                missing.push(i);
+              }
+
+            }
+
+            // Fetch the missing blocks from the blockchain, and update mongo.
+            getBlocksFromBlockchain(missing, (error, blockchainBlocks) => {
+              
+              if (error) {
+                return errors.handleResponse(res, error.message);
+              }
+
+              // Merge the new blocks with the cached blocks.
+              blocks = blocks.concat(blockchainBlocks).sort((a, b) => a._id - b._id);
+
+              return res.json(blocks);
+            });
+
+          });
+
+      });
+
+    });
+
+  /**
    * Get a single block from the blockchain.
    */
   blocksRouter.route('/:id')
